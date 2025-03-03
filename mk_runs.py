6ccc906f484b21e4edc96b4a10c8122083c7f233
Peter Teuben <teuben@gmail.com>
2025-03-03 18:49:11 +0000

    recover a few more obsnums with smaller dv/dw

--- mk_runs.py
+++ mk_runs.py
@@ -10,16 +10,16 @@ project="2024-S1-MX-34"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
-on['NGC4447'] = [-110632,-110637, 
+on['NGC4447'] = [-110632, 110637, 
                   125295, 125297, 125301,-125303,      # 21-jan-2025
                   125362, 125364, 125366,
-		 -125647,-125649,-125747,-125749,-125769,-125771,-125773,-125775,
+		  125647, 125649, 125747, 125749, 125769,-125771,-125773,-125775,
                  ]
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 #pars1['NGC4447']   = "dv=100 dw=150 pix_list=0,2 b_order=3 map_coord_use=0 bank=0"
-pars1['NGC4447']   = "dv=100 dw=150 pix_list=0,1,2,3 b_order=3"
+pars1['NGC4447']   = "vlsr=230 dv=30 dw=100 pix_list=0,1,2,3 b_order=3"
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
